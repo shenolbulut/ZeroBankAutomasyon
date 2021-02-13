79e9c6585a6b55a55415777cb7f610df5086929e
ilyastunch <iltun85@gmail.com>
2021-02-13 12:48:41 +0300

    completed ilyas

--- src/test/java/com/zerobank/step_definitions/AccountActivityNavigationDef.java
+++ src/test/java/com/zerobank/step_definitions/AccountActivityNavigationDef.java
@@ -15,96 +15,79 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
 public class AccountActivityNavigationDef {
+    LoginPage loginPage= new LoginPage();
+    AccountSummaryPage accountSummaryPage= new AccountSummaryPage();
+    AccountActivityPage accountActivityPage= new AccountActivityPage();
+    Select select;
+
+
+    @Given("the user is	on the page")
+    public void the_user_is_on_the_page() {
+       Driver.get().get(ConfigurationReader.get("url"));
+       new FirstPage().signBtn.click();
+       loginPage.login();
+       loginPage.advancedBtn.click();
+       loginPage.proceedToLink.click();
+
+    }
 
     @When("the	user	clicks	on	Savings	link	on	the	Account	Summary	page")
     public void the_user_clicks_on_Savings_link_on_the_Account_Summary_page() {
-        new DashportPage().navigateModule("Online Banking");
-        new OnlineBanking().tabs("Account Summary");
-        new AccountSummaryPage().savingsBtn.click();
+        accountSummaryPage.savingsBtn.click();
     }
 
     @Then("the	Account	Activity page	should	be	displayed")
     public void the_Account_Activity_page_should_be_displayed() {
-        Assert.assertTrue(new OnlineBanking().accountActivity.isDisplayed());
+       Assert.assertTrue(Driver.get().getTitle().contains("Account Activity"));
     }
 
     @Then("Account	drop	down	should	have	Savings	selected")
     public void account_drop_down_should_have_Savings_selected() {
-        Select select=new Select(new AccountActivityPage().dropdown);
-        String expected="Savings";
-        String actual=select.getFirstSelectedOption().getText();
-        Assert.assertEquals(expected,actual);
+       Select select=new Select(accountActivityPage.dropdown);
+       Assert.assertTrue(select.getFirstSelectedOption().getText().contains("Savings"));
+
     }
 
     @When("the	user	clicks	on	Brokerage	link	on	the	Account	Summary	page")
     public void the_user_clicks_on_Brokerage_link_on_the_Account_Summary_page() {
-        BrowserUtils.waitFor(1);
-        new DashportPage().navigateModule("Online Banking");
-        new OnlineBanking().tabs("Account Summary");
-        new AccountSummaryPage().brokerage.click();
+       accountSummaryPage.brokerage.click();
     }
 
     @Then("Account	drop	down	should	have	Brokerage selected")
     public void account_drop_down_should_have_Brokerage_selected() {
-       Select select=new Select(new AccountActivityPage().dropdown);
-       String expected="Brokerage";
-       String actual=select.getFirstSelectedOption().getText();
-       Assert.assertEquals(expected, actual);
+      Select select=new Select(accountActivityPage.dropdown);
+      Assert.assertTrue(select.getFirstSelectedOption().getText().contains("Brokerage"));
     }
 
     @When("the	user	clicks	on	Checking	link	on	the	Account	Summary	page")
     public void the_user_clicks_on_Checking_link_on_the_Account_Summary_page() {
-        new DashportPage().navigateModule("Online Banking");
-        new OnlineBanking().tabs("Account Summary");
-        new AccountSummaryPage().checking.click();
-
+        accountSummaryPage.checking.click();
     }
 
     @Then("Account	drop	down	should	have	Checking selected")
     public void account_drop_down_should_have_Checking_selected() {
-        Select select=new Select(new AccountActivityPage().dropdown);
-        String expected="Checking";
-        String actual=select.getFirstSelectedOption().getText();
-        Assert.assertEquals(expected, actual);
+        Select select= new Select(accountActivityPage.dropdown);
+        Assert.assertTrue(select.getFirstSelectedOption().getText().contains("Checking"));
     }
 
     @When("the	user	clicks	on	Credit	card	link	on	the	Account	Summary	page")
     public void the_user_clicks_on_Credit_card_link_on_the_Account_Summary_page() {
-        new DashportPage().navigateModule("Online Banking");
-        new OnlineBanking().tabs("Account Summary");
-        new AccountSummaryPage().creditCart.click();
+        accountSummaryPage.creditCart.click();
     }
 
     @Then("Account	drop	down	should	have	Credit	Card selected")
     public void account_drop_down_should_have_Credit_Card_selected() {
-        Select select=new Select(new AccountActivityPage().dropdown);
-        String expected="Credit Card";
-        String actual=select.getFirstSelectedOption().getText();
-        Assert.assertEquals(expected, actual);
+        Assert.assertTrue(new Select(accountActivityPage.dropdown).getFirstSelectedOption().getText().equals("Credit Card"));
     }
 
     @When("the	user	clicks	on	Loan	link	on	the	Account	Summary	page")
     public void the_user_clicks_on_Loan_link_on_the_Account_Summary_page() {
-        new DashportPage().navigateModule("Online Banking");
-        new OnlineBanking().tabs("Account Summary");
-        new AccountSummaryPage().loan.click();
+        accountSummaryPage.loan.click();
     }
 
     @Then("Account	drop	down	should	have	Loan selected")
     public void account_drop_down_should_have_Loan_selected() {
-        Select select=new Select(new AccountActivityPage().dropdown);
-        String expected="Loan";
-        String actual=select.getFirstSelectedOption().getText();
-        Assert.assertEquals(expected, actual);
-    }
-
-    @Given("the user is	on the page")
-    public void the_user_is_on_the_page() {
-        Driver.get().get(ConfigurationReader.get("url"));
-        new FirstPage().signBtn.click();
-        new LoginPage().login();
-        Driver.get().navigate().back();
-
+        Assert.assertTrue(new Select(accountActivityPage.dropdown).getFirstSelectedOption().getText().equals("Loan"));
     }
 
 }
